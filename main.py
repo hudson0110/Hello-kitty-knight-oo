558a07f088b1ea4768af1220ed11727fffa9b18a
Hudson <hudsoncandido0110@gmail.com>
2024-08-02 14:00:43 -0300

    adicinada imagens

--- main.py
+++ main.py
@@ -3,32 +3,39 @@ import random
 
 pygame.init()
 
-largura_da_janela = 840   #muda o tamanho
-altura_da_janela= 480
+largura_da_janela = 1000   #muda o tamanho
+altura_da_janela= 700
 gameloop = True           #mantem o jogo rodando
+
+grupo_de_desenho = pygame.sprite.Group()  #crio um grupo com minhas sprites
+
+fundo = pygame.sprite.Sprite(grupo_de_desenho)
+fundo.image = pygame.image.load("Imagens/Fundo.jpg")   #add o fundo
+fundo.rect = pygame.Rect(0,0,largura_da_janela,altura_da_janela)
+
+Personagem = pygame.sprite.Sprite(grupo_de_desenho)
+Personagem.image = pygame.image.load("Imagens/Personagem.png") #add a hello kitty
+Personagem.rect = pygame.Rect(50,552,50,50)
 
 
 display = pygame.display.set_mode([largura_da_janela,altura_da_janela]) #cria a janela
 
 pygame.display.set_caption("Hello Kitty Knight")  #nome da janela
 
-def desenhar():
-    display.fill([255,192,203])
-
 if __name__ == "__main__":
     while gameloop:
         for evento in pygame.event.get():       #verifica se a tecla foi apertada
             if evento.type == pygame.QUIT:      #aqui az o jogo fecha
                 gameloop = False
-            elif evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_w:    #verifica se a tecla foi apertada
-                    print("funciona")  
+            
         
         keys = pygame.key.get_pressed() #salva na fila quais teclas estao sendo apertadas
 
-        if keys[pygame.K_w]:
-            print("segurando")
+    
 
 
-        desenhar()
+        #desenhar
+        
+        grupo_de_desenho.draw(display) #desenha tudo na tela
+
         pygame.display.update()
